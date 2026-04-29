ecffbd7c1772f6a2d0ff4eab10c0896be7602792
Abhishek Yadav <yabhishekk480@gmail.com>
2026-04-29 22:06:36 +0530

    Created user controller

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -4,12 +4,12 @@ import User from "../models/user.model.js"
 export const getCurrentUser = async (req, res, next) => {
     try {
 
-        const user = await User.findbyId(req.userId)
+        const user = await User.findById(req.userId)
         if (!user) {
             return res.status(404).json({ message: "Failed to fetch current user" })
 
         }
-        return response.status(200).json(user)
+        return res.status(200).json(user)
     } catch (error) {
         return res.status(400).json({ message: ` controller  server  error ${error} ` })
     }
