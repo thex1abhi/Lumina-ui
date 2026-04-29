88a61b579e39d4867c9005924fbb60aad081dab0
Abhishek Yadav <yabhishekk480@gmail.com>
2026-04-29 22:07:00 +0530

    Added redux and fetched user

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const googleAuth = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            sameSite: "strict",
+            sameSite: "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -31,7 +31,7 @@ export const logout = async (req, res) => {
         await res.clearCookie("token", {
             httpOnly: true,
             secure: false,
-            sameSite: "strict",
+            sameSite: "lax",
         })
         return res.status(200).json({ message: "Logout successfull " })
     } catch (error) {
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,25 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "./redux/userSlice";
 
 
 export const ServerUrl = "http://localhost:8000"
 function App() {
+
+  const dispatch = useDispatch()
+
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const res = await axios.get(ServerUrl + "/api/user/current-user", {
           withCredentials: true
         })
-        console.log(res.data)
+        dispatch(setUserData(res.data))
       } catch (error) {
         console.log(error)
+        dispatch(setUserData(null))
       }
     }
     fetchUser()
