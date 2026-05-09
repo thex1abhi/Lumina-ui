11e749ca0c879b9d1ef56a83cfce9efca6cdb93a
Abhishek Yadav <yabhishekk480@gmail.com>
2026-05-09 23:41:40 +0530

    Added component schema

---
+++ Server/models/component.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const componentSchema = new mongoose.Schema({
+
+    name: String,
+    code: String,
+    props: [String],
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    visibility: {
+        type: String,
+        enum: ["private", "public"],
+        default: "private"
+    },
+    npmPackage: String
+
+
+}, { timestamps: true })
+
+const Component = mongoose.model("Component", componentSchema)
+
+export default Component;
