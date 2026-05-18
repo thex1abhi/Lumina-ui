d64810ec92dd49acf8ce4ca7a69bbb9afbcb8a18
Abhishek Yadav <yabhishekk480@gmail.com>
2026-05-18 23:38:54 +0530

    Setting openrouter

--- Server/utils/openRouter.js
+++ Server/utils/openRouter.js
@@ -6,7 +6,11 @@ export const askAI = async (messages) => {
             throw new Error("Message array is empty");
         }
         const response = await axios.post("https://openrouter.ai/api/v1/chat/completions", {
-            
+            model:"deepseek/deepseek-chat",
+            messages:messages,
+            temperature:0.7,
+            max_tokens:2000,
+            response_format:{type:"json_object"}
         })
 
     } catch (error) {
