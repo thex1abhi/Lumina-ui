f6736d5659aedd17713e4136836a0a2b5b3f1590
Abhishek Yadav <yabhishekk480@gmail.com>
2026-05-19 23:52:13 +0530

    Added post router for AI

--- Server/utils/openRouter.js
+++ Server/utils/openRouter.js
@@ -6,15 +6,27 @@ export const askAI = async (messages) => {
             throw new Error("Message array is empty");
         }
         const response = await axios.post("https://openrouter.ai/api/v1/chat/completions", {
-            model:"deepseek/deepseek-chat",
-            messages:messages,
-            temperature:0.7,
-            max_tokens:2000,
-            response_format:{type:"json_object"}
-        })
-
+            model: "deepseek/deepseek-chat",
+            messages: messages,
+            temperature: 0.7,
+            max_tokens: 2000,
+            response_format: { type: "json_object" }
+        }, {
+            headers: {
+                Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+                "X-OpenRouter-Title": "LuminaUI",
+                "Content-Type": "application/json",
+            },
+        }
+        ),
+        const content = response?.data?.choices?.[0]?.message?.content
+        if (!content || !content.trim()) {
+            throw new Error("AI returned empty response")
+        }
+        return content
     } catch (error) {
-
+        console.log("OpenRouter error:",error.response?.data || error.message);
+        throw new Error("Open router API error")
     }
 
 }
