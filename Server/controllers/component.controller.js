86b4c9dd1c97186d4eee7a9cc5f904ffc02dce46
Abhishek Yadav <yabhishekk480@gmail.com>
2026-05-22 21:30:40 +0530

    code to save component

--- Server/controllers/component.controller.js
+++ Server/controllers/component.controller.js
@@ -9,12 +9,37 @@ export const saveComponent = async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: "User not found" })
         }
+        //check for admin role
         if (user.role === "admin") {
             const existing = await Component.findOne({ name, visibility: "public" })
+            if (existing) {
+                return res.status(400).json({
+                    message: "Admin cannot create duplicate public component name"
+                });
+            }
         }
-
+        // checking for user role
+        if (user.role !== "admin") {
+            const existing = await Component.findOne({
+                name,
+                owner: req.userId,
+            })
+            if (existing) {
+                return res.status(400).json({
+                    message: "You already have a component with this name"
+                });
+            }
+        }
+        const component = await Component.create({
+            name,
+            code,
+            props,
+            owner: req.userId,
+        }) 
+        return res.status(200).json(component)
     } catch (error) {
+        return res.status(500).json({ message: `Failed to save component ${error}  ` })
+    } 
+} 
 
-    }
 
-}
