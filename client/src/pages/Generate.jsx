feb9164f7dff95e3b40c6ce00828e93678a0d563
Abhishek Yadav <yabhishekk480@gmail.com>
2026-06-05 19:58:01 +0530

    Generate page

--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -2,7 +2,10 @@
  import React from "react";
  
  function Generate() {
-   return <div>Generate</div>;
+   return <div  className="min-h-screen  text-white relative overflow-hidden" > 
+    
+
+   </div>;
  }
  
  export default Generate;
