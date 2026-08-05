228a726398a4cc0996f8da63b7ab13e307360cfc
Abhishek Yadav <yabhishekk480@gmail.com>
2026-08-05 23:10:35 +0530

    Razorpay payment model

---
+++ Server/models/payment.model.js
@@ -0,0 +1,25 @@
+
+import mongoose from "mongoose";
+
+const paymentSchema = mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    amount: Number,
+    aiCredits: Number,
+    razorpayOrderId: String,
+    razorpayPaymentId: String,
+    status: {
+        type: String,
+        enum: ["created", "paid", "failed"],
+        default: "created",
+    },
+
+}, { timestamps: true })
+
+const Payment = mongoose.model("Payment", paymentSchema)
+
+export default Payment;
